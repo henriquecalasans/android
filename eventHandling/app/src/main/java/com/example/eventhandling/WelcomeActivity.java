f713aa105bf84328a149768740826420f7c63b5a
henriquecalasans <henrique_cala@hotmail.com>
2020-06-15 21:52:01 -0300

    Action bar deployed

--- eventHandling/app/src/main/java/com/example/eventhandling/WelcomeActivity.java
+++ eventHandling/app/src/main/java/com/example/eventhandling/WelcomeActivity.java
@@ -4,7 +4,12 @@ package com.example.eventhandling;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+
+import java.util.Objects;
 
 public class WelcomeActivity extends DebugActivity {
 
@@ -17,17 +22,36 @@ public class WelcomeActivity extends DebugActivity {
         //Get intent
         Intent intent = getIntent();
 
-        //Read parameters
+        //Read key
         Bundle args = intent.getExtras();
 
-        //Recieve Bundle key
+        //Read Key value
         String name = args.getString("name");
 
         //Update view with Welcome message text
         TextView tw = findViewById(R.id.tw_welcome);
         String welcome = getString(R.string.welcome);
         tw.setText(welcome + ", " + name);
+
+
+        //Enable action bar
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
 
+        if (id == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 }
